6c6f75d574a7d9ef0d2498aa9658dad3d21fd2dc
Daniel <dhcgn@users.noreply.github.com>
2021-02-07 22:04:05 +0100

    reduce complex logic

--- main.go
+++ main.go
@@ -26,28 +26,26 @@ func main() {
 		log.Fatal("CreateSteam on "+comPath()+": ", err)
 	}
 
-	receivedSerialData := make(chan []byte)
+	//receivedSerialData := make(chan []byte)
 	msg := make(chan []byte)
 
-	go createMessagesFromRawData(receivedSerialData, msg, time.Millisecond*15)
+	// go createMessagesFromRawData(receivedSerialData, msg, time.Millisecond*15)
 	go handleMessages(msg)
-	go listeningRawSerialData(stream, receivedSerialData)
+	go listeningRawSerialData(stream, msg)
 
 	com := communication.New(stream)
 
 	if err := com.RequestConfig(); err != nil {
 		fmt.Println("Error", err)
 	}
-	time.Sleep(time.Millisecond * 30)
+	time.Sleep(time.Millisecond * 500)
 
 	if err := com.RequestVersion(); err != nil {
 		fmt.Println("Error", err)
 	}
-	time.Sleep(time.Millisecond * 30)
+	time.Sleep(time.Millisecond * 500)
 
 	go sendTime(stream)
-	// stream.Write(commands.ReadingOperatingParameters)
-	// stream.Write(commands.MessagerHeader)
 
 	fmt.Println("Waiting for interrupt")
 	select {}
@@ -76,29 +74,10 @@ func handleMessages(msg <-chan []byte) {
 			toString := hex.EncodeToString(m)
 			if len(m) > 3 {
 				fmt.Println("Got msg:", "To:", toString[0:4], "Channel:", toString[4:6], "Msg:", string(m[3:]))
+				break
 			}
 
 			fmt.Println("Got msg:", "0x"+toString)
-		}
-	}
-}
-
-func createMessagesFromRawData(data <-chan []byte, msg chan<- []byte, waitDuration time.Duration) {
-	// TODO Is timer needed, can i use Null-Byte from serial?
-	timer := time.NewTimer(0)
-	timer.Stop()
-
-	buf := make([]byte, 0, 512)
-	for {
-		select {
-		case received := <-data:
-			// fmt.Println("received := <-data")
-			timer = time.NewTimer(waitDuration)
-			buf = append(buf, received...)
-		case <-timer.C:
-			// fmt.Println("<-timer.C")
-			msg <- buf
-			buf = make([]byte, 0, 512)
 		}
 	}
 }
@@ -111,9 +90,9 @@ func sendTime(stream serial.SerialPortIO) {
 		msg = append(msg, 0x0)
 		sendData(stream, msg)
 
-		time.Sleep(time.Second * 1)
+		time.Sleep(time.Second * 2)
 
-		msg = append(commands.MessagerBroadcastHeader, []byte("Hello")...)
+		msg = append(commands.MessagerBroadcastHeader, []byte(fmt.Sprintf("Hello to broadcast from %v", os.Getpid()))...)
 		sendData(stream, msg)
 
 		<-t.C
@@ -135,22 +114,21 @@ func listeningRawSerialData(stream serial.SerialPortIO, data chan []byte) {
 	fmt.Println("Listening loop started")
 
 	buf := make([]byte, 128)
+	msg := make([]byte, 0, 512)
 	for {
-		// TODO Is this necessary? Can I just write and then read?
 		n, err := stream.Read(buf)
 
 		if err != nil {
 			log.Fatal(err)
 		}
-		if n == 0 {
-			continue
+
+		if n > 0 {
+			msg = append(msg, buf[:n]...)
 		}
 
-		// s := string(buf[:n])
-
-		// s := hex.EncodeToString(buf[:n])
-		//fmt.Println(s)
-
-		data <- buf[:n]
+		if n == 0 && len(msg) > 0 {
+			data <- msg
+			msg = make([]byte, 0, 512)
+		}
 	}
 }
